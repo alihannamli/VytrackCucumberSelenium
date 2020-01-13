709db060207b39d32ac04c25bd971527618ec629
Alihan Namli <namlialihan@gmail.com>
2020-01-13 02:23:42 -0500

    smoke

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         features = "src/test/resources/features",
-        glue = "com/vytrack/step_definitions"
+        glue = "com/vytrack/step_definitions",
+        tags = "@smoke"
 
 
 
