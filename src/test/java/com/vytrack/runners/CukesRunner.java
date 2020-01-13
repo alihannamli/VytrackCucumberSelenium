8b56a49318716cf70789aead76cc681ff64057fe
Alihan Namli <namlialihan@gmail.com>
2020-01-13 02:29:05 -0500

    run

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        tags = "@smoke"
+        tags = "@smoke",
+        dryRun = false
 
 
 
