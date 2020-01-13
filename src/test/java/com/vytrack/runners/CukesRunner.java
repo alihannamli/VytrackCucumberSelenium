64dddcfc297a3ee94faeb75cdd0342501894eda7
Alihan Namli <namlialihan@gmail.com>
2020-01-13 03:08:06 -0500

    Login Step Defs

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         features = "src/test/resources/features",
-        glue = "com/vytrack/step_definitions",
-        tags = "@smoke",
-        dryRun = false
+        glue = "com/vytrack/step_definitions"
+       // tags = "@smoke",
+      //  dryRun = true
 
 
 
--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -1,6 +1,24 @@
 package com.vytrack.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class LoginStepDefinitions {
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+
+    @Then("user logs in as store manager")
+    public void user_logs_in_as_store_manager() {
+
+    }
+
+    @Then("user verifies that {string} page subtitle is displayed")
+    public void user_verifies_that_page_subtitle_is_displayed(String string) {
+
+    }
 
 
 
